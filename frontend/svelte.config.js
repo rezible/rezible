a08eb7454fa01b2a69bfec57485d7a5ecbd66cf5
Tex Moran <texm@users.noreply.github.com>
2024-12-29 14:45:01 +0800

    runed, inline bundling

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,6 +4,9 @@ import path from 'path';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		output: {
+			bundleStrategy: "inline",
+		},
 		adapter: adapter({
 			pages: 'dist',
 			assets: 'dist',
