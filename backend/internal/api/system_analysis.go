8659ef5c7a0280c1d821d7c75055187aa6c23846
tex <texm@users.noreply.github.com>
2025-02-05 15:10:38 +0800

    update systemAnalysisHandler method signature to pointer

--- backend/internal/api/system_analysis.go
+++ backend/internal/api/system_analysis.go
@@ -48,19 +48,19 @@ func (s *systemAnalysisHandler) CreateSystemComponent(ctx context.Context, reque
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) GetSystemComponent(ctx context.Context, request *oapi.GetSystemComponentRequest) (*oapi.GetSystemComponentResponse, error) {
+func (s *systemAnalysisHandler) GetSystemComponent(ctx context.Context, request *oapi.GetSystemComponentRequest) (*oapi.GetSystemComponentResponse, error) {
 	var resp oapi.GetSystemComponentResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) UpdateSystemComponent(ctx context.Context, request *oapi.UpdateSystemComponentRequest) (*oapi.UpdateSystemComponentResponse, error) {
+func (s *systemAnalysisHandler) UpdateSystemComponent(ctx context.Context, request *oapi.UpdateSystemComponentRequest) (*oapi.UpdateSystemComponentResponse, error) {
 	var resp oapi.UpdateSystemComponentResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) ArchiveSystemComponent(ctx context.Context, request *oapi.ArchiveSystemComponentRequest) (*oapi.ArchiveSystemComponentResponse, error) {
+func (s *systemAnalysisHandler) ArchiveSystemComponent(ctx context.Context, request *oapi.ArchiveSystemComponentRequest) (*oapi.ArchiveSystemComponentResponse, error) {
 	var resp oapi.ArchiveSystemComponentResponse
 
 	return &resp, nil
@@ -269,7 +269,7 @@ func makeFakeSystemAnalysis() oapi.SystemAnalysis {
 	}
 }
 
-func (s systemAnalysisHandler) GetSystemAnalysis(ctx context.Context, request *oapi.GetSystemAnalysisRequest) (*oapi.GetSystemAnalysisResponse, error) {
+func (s *systemAnalysisHandler) GetSystemAnalysis(ctx context.Context, request *oapi.GetSystemAnalysisRequest) (*oapi.GetSystemAnalysisResponse, error) {
 	var resp oapi.GetSystemAnalysisResponse
 
 	resp.Body.Data = makeFakeSystemAnalysis()
@@ -277,61 +277,61 @@ func (s systemAnalysisHandler) GetSystemAnalysis(ctx context.Context, request *o
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) ListSystemAnalysisComponents(ctx context.Context, request *oapi.ListSystemAnalysisComponentsRequest) (*oapi.ListSystemAnalysisComponentsResponse, error) {
+func (s *systemAnalysisHandler) ListSystemAnalysisComponents(ctx context.Context, request *oapi.ListSystemAnalysisComponentsRequest) (*oapi.ListSystemAnalysisComponentsResponse, error) {
 	var resp oapi.ListSystemAnalysisComponentsResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) AddSystemAnalysisComponent(ctx context.Context, request *oapi.AddSystemAnalysisComponentRequest) (*oapi.AddSystemAnalysisComponentResponse, error) {
+func (s *systemAnalysisHandler) AddSystemAnalysisComponent(ctx context.Context, request *oapi.AddSystemAnalysisComponentRequest) (*oapi.AddSystemAnalysisComponentResponse, error) {
 	var resp oapi.AddSystemAnalysisComponentResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) GetSystemAnalysisComponent(ctx context.Context, request *oapi.GetSystemAnalysisComponentRequest) (*oapi.GetSystemAnalysisComponentResponse, error) {
+func (s *systemAnalysisHandler) GetSystemAnalysisComponent(ctx context.Context, request *oapi.GetSystemAnalysisComponentRequest) (*oapi.GetSystemAnalysisComponentResponse, error) {
 	var resp oapi.GetSystemAnalysisComponentResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) UpdateSystemAnalysisComponent(ctx context.Context, request *oapi.UpdateSystemAnalysisComponentRequest) (*oapi.UpdateSystemAnalysisComponentResponse, error) {
+func (s *systemAnalysisHandler) UpdateSystemAnalysisComponent(ctx context.Context, request *oapi.UpdateSystemAnalysisComponentRequest) (*oapi.UpdateSystemAnalysisComponentResponse, error) {
 	var resp oapi.UpdateSystemAnalysisComponentResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) DeleteSystemAnalysisComponent(ctx context.Context, request *oapi.DeleteSystemAnalysisComponentRequest) (*oapi.DeleteSystemAnalysisComponentResponse, error) {
+func (s *systemAnalysisHandler) DeleteSystemAnalysisComponent(ctx context.Context, request *oapi.DeleteSystemAnalysisComponentRequest) (*oapi.DeleteSystemAnalysisComponentResponse, error) {
 	var resp oapi.DeleteSystemAnalysisComponentResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) ListSystemAnalysisRelationships(ctx context.Context, request *oapi.ListSystemAnalysisRelationshipsRequest) (*oapi.ListSystemAnalysisRelationshipsResponse, error) {
+func (s *systemAnalysisHandler) ListSystemAnalysisRelationships(ctx context.Context, request *oapi.ListSystemAnalysisRelationshipsRequest) (*oapi.ListSystemAnalysisRelationshipsResponse, error) {
 	var resp oapi.ListSystemAnalysisRelationshipsResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) CreateSystemAnalysisRelationship(ctx context.Context, request *oapi.CreateSystemAnalysisRelationshipRequest) (*oapi.CreateSystemAnalysisRelationshipResponse, error) {
+func (s *systemAnalysisHandler) CreateSystemAnalysisRelationship(ctx context.Context, request *oapi.CreateSystemAnalysisRelationshipRequest) (*oapi.CreateSystemAnalysisRelationshipResponse, error) {
 	var resp oapi.CreateSystemAnalysisRelationshipResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) GetSystemAnalysisRelationship(ctx context.Context, request *oapi.GetSystemAnalysisRelationshipRequest) (*oapi.GetSystemAnalysisRelationshipResponse, error) {
+func (s *systemAnalysisHandler) GetSystemAnalysisRelationship(ctx context.Context, request *oapi.GetSystemAnalysisRelationshipRequest) (*oapi.GetSystemAnalysisRelationshipResponse, error) {
 	var resp oapi.GetSystemAnalysisRelationshipResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) UpdateSystemAnalysisRelationship(ctx context.Context, request *oapi.UpdateSystemAnalysisRelationshipRequest) (*oapi.UpdateSystemAnalysisRelationshipResponse, error) {
+func (s *systemAnalysisHandler) UpdateSystemAnalysisRelationship(ctx context.Context, request *oapi.UpdateSystemAnalysisRelationshipRequest) (*oapi.UpdateSystemAnalysisRelationshipResponse, error) {
 	var resp oapi.UpdateSystemAnalysisRelationshipResponse
 
 	return &resp, nil
 }
 
-func (s systemAnalysisHandler) DeleteSystemAnalysisRelationship(ctx context.Context, request *oapi.DeleteSystemAnalysisRelationshipRequest) (*oapi.DeleteSystemAnalysisRelationshipResponse, error) {
+func (s *systemAnalysisHandler) DeleteSystemAnalysisRelationship(ctx context.Context, request *oapi.DeleteSystemAnalysisRelationshipRequest) (*oapi.DeleteSystemAnalysisRelationshipResponse, error) {
 	var resp oapi.DeleteSystemAnalysisRelationshipResponse
 
 	return &resp, nil
