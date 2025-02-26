c2bf7e06cd6e6002972d7608f14886be6bfaa76b
tex <texm@users.noreply.github.com>
2025-02-26 13:58:14 +0800

    backend: fake incident

--- backend/internal/providers/fake/incident.go
+++ backend/internal/providers/fake/incident.go
@@ -14,10 +14,8 @@ import (
 
 type IncidentDataProvider struct {
 	onIncidentUpdatedFn rez.DataProviderResourceUpdatedCallback
-
-	incidentMappingSupport *ent.Incident
-
-	userIdEmails map[string]string
+	fakeRoles           []*ent.IncidentRole
+	fakeIncidents       []*ent.Incident
 }
 
 type IncidentDataProviderConfig struct {
@@ -25,13 +23,75 @@ type IncidentDataProviderConfig struct {
 
 func NewIncidentDataProvider(cfg IncidentDataProviderConfig) (*IncidentDataProvider, error) {
 	p := &IncidentDataProvider{
-		userIdEmails: make(map[string]string),
 		onIncidentUpdatedFn: func(id string, m time.Time) {
 			log.Warn().Msg("no onIncidentUpdated function")
 		},
 	}
 
+	p.makeFakeIncidentRoles()
+	p.makeFakeIncidents()
+
 	return p, nil
+}
+
+func (p *IncidentDataProvider) makeFakeIncidentRoles() {
+	role := &ent.IncidentRole{Name: "Role 1"}
+
+	p.fakeRoles = []*ent.IncidentRole{role}
+}
+
+func (p *IncidentDataProvider) makeFakeIncidents() {
+	severity := &ent.IncidentSeverity{
+		Name:        "Severity 1",
+		Description: "a severity",
+	}
+	incType := &ent.IncidentType{Name: "Default"}
+	tasks := []*ent.Task{{Title: "A Task"}}
+
+	tags := []*ent.IncidentTag{{Key: "foo", Value: "bar"}}
+
+	user := &ent.User{
+		Name:   "User 1",
+		Email:  "user@example.com",
+		ChatID: "foo",
+	}
+
+	roles := []*ent.IncidentRoleAssignment{
+		{Edges: ent.IncidentRoleAssignmentEdges{
+			Role: p.fakeRoles[0],
+			User: user,
+		}},
+	}
+
+	milestones := []*ent.IncidentMilestone{
+		{
+			Type:  "",
+			Time:  time.Now().Add(-8 * time.Hour),
+			Edges: ent.IncidentMilestoneEdges{},
+		},
+	}
+
+	inc1 := &ent.Incident{
+		ProviderID:    "test-incident",
+		Slug:          "test-incident",
+		Title:         "Test Incident",
+		Private:       false,
+		Summary:       "a test incident",
+		OpenedAt:      time.Now().Add(-8 * time.Hour),
+		ModifiedAt:    time.Now().Add(-7 * time.Hour),
+		ClosedAt:      time.Now().Add(-7 * time.Hour),
+		ChatChannelID: "",
+		Edges: ent.IncidentEdges{
+			Severity:        severity,
+			Type:            incType,
+			Tasks:           tasks,
+			RoleAssignments: roles,
+			TagAssignments:  tags,
+			Milestones:      milestones,
+		},
+	}
+
+	p.fakeIncidents = []*ent.Incident{inc1}
 }
 
 func (p *IncidentDataProvider) GetWebhooks() rez.Webhooks {
@@ -52,16 +112,21 @@ func (p *IncidentDataProvider) IncidentRoleDataMapping() *ent.IncidentRole {
 
 func (p *IncidentDataProvider) PullIncidents(ctx context.Context) iter.Seq2[*ent.Incident, error] {
 	return func(yield func(i *ent.Incident, err error) bool) {
-
+		for _, inc := range p.fakeIncidents {
+			yield(inc, nil)
+		}
 	}
 }
 
 func (p *IncidentDataProvider) GetIncidentByID(ctx context.Context, id string) (*ent.Incident, error) {
-	return nil, fmt.Errorf("not implemented")
+	for _, inc := range p.fakeIncidents {
+		if inc.ProviderID == id {
+			return inc, nil
+		}
+	}
+	return nil, fmt.Errorf("not found")
 }
 
 func (p *IncidentDataProvider) GetRoles(ctx context.Context) ([]*ent.IncidentRole, error) {
-	roles := make([]*ent.IncidentRole, 0)
-
-	return roles, nil
+	return p.fakeRoles, nil
 }
