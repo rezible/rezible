a1dff578967917dc77342841fd5e1c0f293678d1
tex <texm@users.noreply.github.com>
2025-03-04 11:34:49 +0800

    fe: update profile menu

--- frontend/src/features/app/components/user-profile-menu/UserProfileMenu.svelte
+++ frontend/src/features/app/components/user-profile-menu/UserProfileMenu.svelte
@@ -1,39 +1,25 @@
 <script lang="ts">
-	import { Popover, Button, Icon, ListItem, ThemeSelect } from "svelte-ux";
-	import { mdiAccount, mdiAbTesting, mdiChevronDown, mdiCog } from "@mdi/js";
+	import { Popover, Button, Icon, ListItem, ThemeSwitch } from "svelte-ux";
+	import { mdiAccount, mdiChevronDown, mdiCog } from "@mdi/js";
 	import { session } from "$lib/auth.svelte";
 	import Avatar from "$components/avatar/Avatar.svelte";
-	import { getToastState } from "$features/app/lib/toasts.svelte";
 
 	const accountLinks = [
 		{ href: "/profile", title: "Profile", icon: mdiAccount },
 		{ href: "/preferences", title: "Preferences", icon: mdiCog },
 	];
 	let accountMenuOpen = $state(false);
-
-	const toasts = getToastState();
 </script>
 
 <Popover bind:open={accountMenuOpen}>
-	<div class="bg-surface-100 border shadow flex flex-col gap-1 p-2">
+	<div class="bg-surface-100 border shadow flex flex-col gap-1 p-2 items-center">
 		<span>{session.username}</span>
 		{#each accountLinks as l}
-			<a href={l.href} class="">
+			<a href={l.href} class="w-full">
 				<ListItem title={l.title} icon={l.icon} classes={{ root: "hover:bg-accent" }} />
 			</a>
 		{/each}
-		<Button
-			on:click={() =>
-				toasts.add(
-					"test",
-					"an example toast",
-					Math.random() > 0.25 ? mdiAbTesting : undefined,
-					1000000
-				)}
-		>
-			add toast
-		</Button>
-		<ThemeSelect />
+		<ThemeSwitch />
 	</div>
 </Popover>
 
