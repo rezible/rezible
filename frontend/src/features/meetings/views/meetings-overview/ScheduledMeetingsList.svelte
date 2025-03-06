1cff50056e500ff0ff0e4ba32fe9cc42cdce35cd
tex <texm@users.noreply.github.com>
2025-03-06 11:33:34 +0800

    update scheduled meetings card

--- frontend/src/features/meetings/views/meetings-overview/ScheduledMeetingsList.svelte
+++ frontend/src/features/meetings/views/meetings-overview/ScheduledMeetingsList.svelte
@@ -3,15 +3,22 @@
 		listMeetingSchedulesOptions,
 		type ListMeetingSchedulesData,
 		type MeetingSchedule,
+		type MeetingScheduleTiming,
 	} from "$lib/api";
 	import { createQuery } from "@tanstack/svelte-query";
-	import { Button, Collapse, Header, Icon } from "svelte-ux";
+	import { Button, ListItem, Header, Icon } from "svelte-ux";
 	import LoadingQueryWrapper from "$components/loader/LoadingQueryWrapper.svelte";
-	import { mdiChevronDown, mdiFilter } from "@mdi/js";
+	import { mdiChevronDown, mdiChevronRight, mdiFilter } from "@mdi/js";
 	import ScheduledMeetingCard from "$features/meetings/components/scheduled-meeting-card/ScheduledMeetingCard.svelte";
+	import { addMinutes } from "date-fns";
 
 	let queryParams = $state<ListMeetingSchedulesData["query"]>({});
 	const query = createQuery(() => listMeetingSchedulesOptions({ query: { ...queryParams } }));
+
+	const getScheduleTimeDisplay = (m: MeetingScheduleTiming) => {
+		// TODO: Implement this
+		return addMinutes(Date.now(), 60).toLocaleString();
+	};
 </script>
 
 <div class="flex flex-col gap-2 min-h-0 h-full">
@@ -25,12 +32,24 @@
 			</Button>
 		</div>
 	</Header>
-	
+
 	<div class="flex-1 flex flex-col gap-2 overflow-y-auto">
 		<LoadingQueryWrapper {query}>
 			{#snippet view(schedules: MeetingSchedule[])}
-				{#each schedules as schedule}
-					<ScheduledMeetingCard {schedule} />
+				{#each schedules as sched}
+					<a href="/meetings/scheduled/{sched.id}">
+						<ListItem
+							title={sched.attributes.name}
+							subheading={getScheduleTimeDisplay(sched.attributes.timing)}
+							href={`/meetings/scheduled/${sched.id}`}
+							classes={{ root: "border first:border-t rounded elevation-0 group hover:bg-primary-100/10" }}
+							noShadow
+						>
+							<div slot="actions" class="group-hover:text-primary">
+								<Icon data={mdiChevronRight} />
+							</div>
+						</ListItem>
+					</a>
 				{/each}
 			{/snippet}
 		</LoadingQueryWrapper>
