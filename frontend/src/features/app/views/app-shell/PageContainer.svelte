25687925cc18e7d1a7e8c99aa320f4fbbd16d263
tex <texm@users.noreply.github.com>
2025-04-01 14:04:30 +0800

    fe: cleanup pagecontainer breadcrumbs

--- frontend/src/features/app/views/app-shell/PageContainer.svelte
+++ frontend/src/features/app/views/app-shell/PageContainer.svelte
@@ -1,36 +1,37 @@
 <script lang="ts">
 	import type { Snippet } from "svelte";
-	import { appShell } from "$features/app/lib/appShellState.svelte";
+	import { appShell, type PageBreadcrumb } from "$features/app/lib/appShellState.svelte";
 	import Avatar from "$components/avatar/Avatar.svelte";
 
 	type Props = {children: Snippet};
 	const { children }: Props = $props();
 
+	const pageBreadcrumbs = $derived(appShell.breadcrumbs);
 	const pageActions = $derived(appShell.pageActions);
 	const propsFn = $derived(pageActions?.propsFn ?? (() => ({})));
 	const pageActionsProps = $derived.by(() => (propsFn()));
 </script>
 
+{#snippet breadcrumb(c: PageBreadcrumb, last: boolean)}
+	<span class="flex items-stretch gap-2">
+		{#if c.avatar}
+			<Avatar {...c.avatar} size={30} />
+		{/if}
+
+		{#if c.href}
+			<a href={c.href} class:text-3xl={last} class:text-surface-content={last}>{c.label ?? "loading"}</a>
+		{:else}
+			<span class:text-3xl={last} class:text-surface-content={last}>{c.label ?? "loading"}</span>
+		{/if}
+	</span>
+{/snippet}
+
 <div class="w-full max-w-full h-full max-h-full min-h-0 overflow-hidden flex flex-col gap-2 px-2 pb-2 border rounded-md bg-surface-200 text-surface-content">
 	<div class="border-b flex justify-between items-bottom h-11">
 		<span class="text-xl text-surface-content/50 w-fit self-bottom flex gap-1 items-end px-1">
-			{#each appShell.breadcrumbs as c, i}
-				{@const last = i === appShell.breadcrumbs.length - 1}
-				{#if i > 0}
-					<span>/</span>
-				{/if}
-		
-				<span class="flex items-stretch gap-2">
-					{#if c.avatar}
-						<Avatar {...c.avatar} size={30} />
-					{/if}
-		
-					{#if c.href}
-						<a href={c.href} class:text-3xl={last} class:text-surface-content={last}>{c.label ?? "loading"}</a>
-					{:else}
-						<span class:text-3xl={last} class:text-surface-content={last}>{c.label ?? "loading"}</span>
-					{/if}
-				</span>
+			{#each pageBreadcrumbs as c, i}
+				{#if i > 0}<span>/</span>{/if}
+				{@render breadcrumb(c, i === pageBreadcrumbs.length - 1)}
 			{/each}
 		</span>
 
