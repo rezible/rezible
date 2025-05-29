bcee997309bdcf29a54f1e20ae7a47b8d74d768e
tex <texm@users.noreply.github.com>
2025-05-29 13:01:06 +0800

    fe: remove unused dependencies & timezone map

--- frontend/src/features/reports/views/report-view/IncidentsGraph.svelte
+++ frontend/src/features/reports/views/report-view/IncidentsGraph.svelte
@@ -2,19 +2,7 @@
 	import { startOfMonth, endOfMonth } from "date-fns";
 	import { Button, Header } from "svelte-ux";
 	import { mdiDotsVertical } from "@mdi/js";
-	import { createDateSeries } from "../../lib/genData";
 
-	const now = new Date();
-	const firstDayOfMonth = startOfMonth(now);
-	const lastDayOfMonth = endOfMonth(now);
-
-	const dateSeriesData = createDateSeries({
-		count: 10,
-		min: 20,
-		max: 100,
-		value: "integer",
-		keys: ["value", "baseline"],
-	});
 </script>
 
 <div class="h-fit flex flex-col gap-2 border rounded px-4 py-2">
@@ -28,48 +16,6 @@
 	</Header>
 
 	<div class="h-[300px] p-4 border rounded bg-surface-100">
-		<!--BarChart
-			data={fakeDateSeries}
-			x="date"
-			y="value"
-			renderContext="svg"
-			debug={false}
-			c="value"
-			cRange={[
-				"oklch(var(--color-success))",
-				"oklch(var(--color-warning))",
-				"oklch(var(--color-danger))",
-			]}
-		/-->
-		<!--BarChart data={fakeDateSeries} x="date" y="value" renderContext="canvas" debug={false} tooltip={false}>
-			<svelte:fragment slot="marks" let:series let:getBarsProps>
-				{#each series as s, i (s.key)}
-					<LinearGradient
-						class="from-danger-500 to-success-400"
-						vertical
-						units="userSpaceOnUse"
-						let:gradient
-					>
-						<Bars {...getBarsProps(s, i)} fill={gradient} />
-					</LinearGradient>
-				{/each}
-			</svelte:fragment>
-		</BarChart-->
+		
 	</div>
-
-	<!--div class="h-64">
-		<BarChart
-			data={dateData}
-			x="date"
-			y="value"
-			grid={{ x: true, y: true }}
-			tooltip={false}
-			cRange={[
-				"hsl(var(--color-danger))",
-				"hsl(var(--color-warning))",
-				"hsl(var(--color-success))",
-				"hsl(var(--color-info))",
-			]}
-		/>
-	</div-->
 </div>
