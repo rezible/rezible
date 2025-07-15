859a97678fc341d46b49071b5674ff1e72ce3e62
tex <texm@users.noreply.github.com>
2025-07-15 17:41:14 +0800

    fe: update users feature dir

--- frontend/src/routes/users/[id]/+page.svelte
+++ frontend/src/routes/users/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from "$app/state";
-	import UserProfileView from "$features/user/views/user-profile/UserProfileView.svelte";
+	import UserProfileView from "$features/users/views/user-profile/UserProfileView.svelte";
 </script>
 
 <UserProfileView userId={page.params.id} />
