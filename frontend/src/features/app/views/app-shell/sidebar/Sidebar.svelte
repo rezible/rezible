c56e5eca69ea0ae31a1f95014345fec0d42d871e
tex <texm@users.noreply.github.com>
2025-07-15 17:56:26 +0800

    fe: sidebar routes

--- frontend/src/features/app/views/app-shell/sidebar/Sidebar.svelte
+++ frontend/src/features/app/views/app-shell/sidebar/Sidebar.svelte
@@ -11,6 +11,7 @@
 		mdiFire,
 		mdiHome,
 		mdiPhoneRing,
+		mdiShape,
 		mdiShield,
 		mdiVectorPolyline,
 		mdiVideo,
@@ -22,21 +23,27 @@
 	import OmniSearch from "./omni-search/OmniSearch.svelte";
 	import UserProfileMenu from "./UserProfileMenu.svelte";
 
-	type SidebarItem = {
+	type SidebarRoute = {
 		path: string;
 		route?: string;
 		label: string;
 		icon: string;
 	};
-	type SidebarNavItem = SidebarItem | SidebarItem & { children: SidebarItem[] };
-	const routes: SidebarNavItem[] = [
+	type SidebarGroup = {
+		label: string;
+	}
+	type SidebarItem = SidebarRoute | SidebarGroup;
+	const items: SidebarItem[] = [
 		{ path: "/", route: "/(index)", label: "Home", icon: mdiHome },
+		{ label: "Oncall" },
 		{ path: "/incidents", label: "Incidents", icon: mdiFire },
-		{ path: "/shifts", label: "Shifts", icon: mdiAccountClock },
-		{ path: "/rosters", label: "Rosters", icon: mdiShield },
-		{ path: "/playbooks", label: "Playbooks", icon: mdiBookshelf },
 		{ path: "/alerts", label: "Alerts", icon: mdiAlarmLight },
-		{ path: "/system", label: "System", icon: mdiVectorPolyline },
+		{ path: "/rosters", label: "Rosters", icon: mdiShield },
+		{ path: "/shifts", label: "Shifts", icon: mdiAccountClock },
+		{ label: "System" },
+		{ path: "/playbooks", label: "Playbooks", icon: mdiBookshelf },
+		{ path: "/system", label: "Components", icon: mdiShape },
+		{ label: "People" },
 		{ path: "/teams", label: "Teams", icon: mdiAccountGroup },
 		{ path: "/meetings", label: "Meetings", icon: mdiVideo },
 	];
@@ -47,12 +54,12 @@
     const preloadHome = $derived(session.error ? "tap" : "hover");
 </script>
 
-{#snippet navItem(r: SidebarItem)}
+{#snippet navRouteItem(r: SidebarRoute)}
 	{@const active = currentPath?.startsWith(r.route ?? r.path)}
 	<a
 		href={r.path}
 		class={cls(
-			"inline-block px-4 py-3 flex items-center gap-2 text-center border-none-2 rounded-lg",
+			"inline-block px-4 py-2 flex items-center gap-2 text-center border-none-2 rounded-lg",
 			active
 				? "text-neutral-content bg-primary-900"
 				: "border-transparent hover:text-primary-content hover:border-primary/50 hover:bg-primary-900/50"
@@ -84,28 +91,12 @@
 	</div>
 
 	<div class="overflow-y-auto flex flex-col flex-1 min-h-0 justify-between">
-		<div class="flex flex-col gap-2 overflow-y-auto overflow-x-hidden">
-			{#each routes as r (r.label)}
-				{#if "children" in r}
-					<Collapse>
-						<div
-							slot="trigger"
-							class="inline-block p-4 flex flex-1 items-center gap-2 text-center"
-						>
-							<Icon
-								data={r.icon}
-								classes={{ root: "mr-3" }}
-							/>
-							{r.label}
-						</div>
-						<div class="ml-4">
-							{#each r.children as rc (rc.label)}
-								{@render navItem(rc)}
-							{/each}
-						</div>
-					</Collapse>
+		<div class="flex flex-col overflow-y-auto gap-1 overflow-x-hidden">
+			{#each items as item}
+				{#if "path" in item}
+					{@render navRouteItem(item)}
 				{:else}
-					{@render navItem(r)}
+					<span class="font-bold text-sm uppercase text-surface-content/40 pl-2 mt-3">{item.label}</span>
 				{/if}
 			{/each}
 		</div>
